ab548f2b4210fff224cd3f23c837ccf87ec38b1c
farjam <farjamhargolan@gmail.com>
2023-09-15 18:41:50 +0330

    Delete useless project.

--- SupplyService/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SupplyService.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
